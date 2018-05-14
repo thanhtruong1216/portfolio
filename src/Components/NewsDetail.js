f4f2c06d05290c213706a6d0e1db3ae40881851c
Thanh Thanh <thanhtruong1216@gmail.com>
2018-05-15 01:58:48 +0700

    Feat: Create NewsDetail component

---
+++ src/Components/NewsDetail.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../Styles/NewsDetail.css';
+
+class NewsDetail extends Component {
+  render() {
+    return(
+      <section className="news-detail-container">
+        This is News Detail
+      </section>
+    );
+  }
+}
+
+export default NewsDetail;
