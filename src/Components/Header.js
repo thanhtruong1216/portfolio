5e9cfcc840c750e0fd0de28a485ab98fcc61e398
Thanh Thanh <thanhtruong1216@gmail.com>
2018-05-15 01:55:16 +0700

    Feat: Create Header component

---
+++ src/Components/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../Styles/Header.css';
+
+class Header extends Component {
+  render() {
+    return(
+      <section className="navigation-container">
+        <img src="#" alt="interdist-logo"/>
+        <nav>This is navigation menu</nav>
+      </section>
+    );
+  }
+}
+export default Header;
