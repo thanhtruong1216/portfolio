5c57404135117174064c3372dda200af15ba6a78
Thanh Thanh <thanhtruong1216@gmail.com>
2018-05-15 01:57:15 +0700

    Feat: Create News component

---
+++ src/Components/News.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import '../Styles/News.css';
+
+class News extends Component {
+  render() {
+    return(
+      <section className="news-container">
+        This is News
+      </section>
+    );
+  }
+}
+export default News;
+
+{/*<article>
+          <img src="#" alt="story-image"/>
+          <h4 className="story-title">This is title of story</h4>
+          <p className="story-lead">This is chapeau of story</p>
+        </article>*/}
