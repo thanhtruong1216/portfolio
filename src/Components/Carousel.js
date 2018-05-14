0bbb41e6e8bc24f8d32f43af97bdb12da978721a
Thanh Thanh <thanhtruong1216@gmail.com>
2018-05-15 01:56:24 +0700

    Feat: Create Carousel component

---
+++ src/Components/Carousel.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../Styles/Carousel.css';
+
+class Carousel extends Component {
+  render() {
+    return(
+      <section className="carousel-container">
+        This is Carousel
+      </section>
+    );
+  }
+}
+
+export default Carousel;
