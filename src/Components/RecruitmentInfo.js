156aa905ef82174ac76a60cd723530e12e57a8d8
Thanh Thanh <thanhtruong1216@gmail.com>
2018-05-15 01:59:23 +0700

    Feat: Create RecruitmentInfo component

---
+++ src/Components/RecruitmentInfo.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import '../Styles/RecruitmentInfo.css'
+
+class RecruitmentInfo extends Component {
+  render() {
+    return(
+      <section className="recruitment-info-container">
+        This is Recruitment Information
+      </section>
+    );
+  }
+}
+export default RecruitmentInfo;
