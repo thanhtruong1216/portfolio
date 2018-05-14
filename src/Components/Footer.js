18fbc2951bf5849b8c35ea2daa5f4134fc558431
Thanh Thanh <thanhtruong1216@gmail.com>
2018-05-15 01:55:45 +0700

    Feat: Create Footer component

---
+++ src/Components/Footer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../Styles/Footer.css';
+
+class Footer extends Component {
+  render() {
+    return(
+      <section className="contact-info-container">
+        This is Interdist contact imformation
+      </section>
+    );
+  }
+}
+
+export default Footer;
