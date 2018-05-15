be11cd8d4268c6dc76d707c5e7af5f19eeb6bcdb
Thanh Thanh <thanhtruong1216@gmail.com>
2018-05-15 19:49:25 +0700

    Add Link to Recruitment page

--- src/Components/RecruitmentInfo.js
+++ src/Components/RecruitmentInfo.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import '../styles/RecruitmentInfo.css'
 
 class RecruitmentInfo extends Component {
@@ -6,6 +7,7 @@ class RecruitmentInfo extends Component {
     return(
       <section className="recruitment-info-container">
         This is Recruitment Information
+        <Link to="/recruitment">Show more</Link>
       </section>
     );
   }
