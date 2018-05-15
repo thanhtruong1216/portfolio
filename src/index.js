11fce493fff30443d7a0befd47812b7a7c07000e
Thanh Thanh <thanhtruong1216@gmail.com>
2018-05-15 16:30:44 +0700

    Create route for components

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import NewsDetail from './components/NewsDetail';
+import RecruitmentInfo from './components/RecruitmentInfo';
 import registerServiceWorker from './registerServiceWorker';
-import { createStore } from 'redux';
 import createHistory from 'history/createBrowserHistory';
 import reducers from './reducers';
 import { Provider } from 'react-redux';
@@ -11,14 +13,27 @@ import { createStore, applyMiddleware } from 'redux';
 import { Route } from 'react-router-dom';
 import { ConnectedRouter, routerMiddleware } from 'react-router-redux';
 
+
+const history = createHistory();
+const middleware = routerMiddleware(history);
 const store = createStore(
   reducers,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(middleware)
 );
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
-  </Provider>, document.getElementById('root'));
+    <ConnectedRouter history={history}>
+      <div>
+        <Route exact path="/" component={App} />
+        <Route path="/news" component={NewsDetail} />
+        <Route path="/recruitment" component={RecruitmentInfo} />
+      </div>
+    </ConnectedRouter>
+  </Provider>,
+  document.getElementById('root')
+);
+
 registerServiceWorker();
 
