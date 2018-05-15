2d76708ec7dc984c5dbd7470d07afe1c2eb82f75
Thanh Thanh <thanhtruong1216@gmail.com>
2018-05-15 14:15:58 +0700

    Feat: Create Redux store and and redux dev tools

--- src/index.js
+++ src/index.js
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore } from 'redux';
+import reducers from './reducers';
+
+const store = createStore(
+  reducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
