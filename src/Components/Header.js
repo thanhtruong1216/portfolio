7da08495d6de352abde5eb15252ad69aa38980be
Thanh Thanh <thanhtruong1216@gmail.com>
2018-05-15 19:51:39 +0700

    Feat: Create navigation and add links to componnents.

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import '../styles/Header.css';
 
 class Header extends Component {
   render() {
     return(
-      <section className="navigation-container">
-        <img src="#" alt="interdist-logo"/>
-        <nav>This is navigation menu</nav>
+      <section className="header-container">
+        <div className="logo-wrapper">
+          <img src="#" alt="interdist-logo" />
+        </div>
+        <nav className="navigation-bar">
+          <Link to="/">Home</Link>
+          <Link to="/news">News</Link>
+          <Link to="/introduction">Introduction</Link>
+          <Link to="/recruitment">Recruitment</Link>
+          <Link to="/contact">Contact</Link>
+        </nav>
       </section>
     );
   }
