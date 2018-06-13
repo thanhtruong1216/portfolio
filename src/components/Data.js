d4f5cd9fff263ca3b887ecb58d7b2e62c0bb28bd
Thanh Thanh <thanhtruong1216@gmail.com>
2018-06-13 23:44:44 +0700

    Feat: Change content of data variable

--- src/components/Data.js
+++ src/components/Data.js
@@ -1,3 +1,5 @@
+import matching from '../images/matching.png';
+import map from '../images/map.png';
 const width = '200px';
 const height = '200px';
 const margin = '20px';
@@ -8,60 +10,39 @@ const data = {
       id: 1,
       name: 'readable',
       link: 'readable',
-      styles: {
-        margin: margin,
-        width: width,
-        height: height,
-        background: 'linear-gradient(#76b852,#8DC26F)'
-      },
+      image: matching,
+      styles: {},
       skills: ['Redux','Fetch API', 'Babel', 'Webpack', 'NPM', 'Git' ]
     },
     {
       id: 2,
       name: 'memory game',
       link: "memorygame",
-      styles: {
-        margin: margin,
-        width: width,
-        height: height,
-        background: 'linear-gradient(0.15turn, #00c6ff, #0072ff)'
-      },
+      image: matching,
+      styles: {},
       skills: ['HTML', 'CSS', 'Javascript']
     },
     {
       id: 3,
       name: 'book tracking app',
       link: 'booktracking',
-      styles: {
-        margin: margin,
-        width: width,
-        height: height,
-        background: 'linear-gradient(0.15turn, #ff0084, #33001b)'
-      },
+      image: matching,
+      styles: {},
       skills: ['React', 'HTML', 'CSS', 'ES6 JavaScript', 'Asynchronous JavaScript', 'Fetch API', 'Babel', 'Webpack' ]
     },
     {
       id: 4,
       name: 'neighborhood map',
       link: 'neighborhoodmap',
-      styles: {
-        margin: margin,
-        width: width,
-        height: height,
-        background: 'linear-gradient(#43cea2, #185a9d)'
-      },
+      image: map,
       skills: ['React', 'FourSquare API', 'Fetch API', 'Babel', 'NPM', 'Git' ]
     },
     {
       id: 5,
       name: 'mobile flashcard',
       link: 'mobileflashcard',
-      styles: {
-        margin: margin,
-        width: width,
-        height: height,
-        background: 'linear-gradient(0.15turn, #ec6f66, #f3a183)'
-      },
+      image: matching,
+      styles: {},
       skills: ['React Native', 'NPM', 'Babel', 'Expo', 'Git' ]
     }
   ]
