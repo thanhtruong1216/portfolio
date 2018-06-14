cf084fb4ffa1619e3c589628c8f0935dca07e7e2
Thanh Thanh <thanhtruong1216@gmail.com>
2018-06-14 21:18:02 +0700

    Feat: Add education object to data

--- src/components/Data.js
+++ src/components/Data.js
@@ -1,49 +1,76 @@
-import matching from '../images/matching.png';
+import matching from '../images/memory.png';
 import map from '../images/map.png';
-const width = '200px';
-const height = '200px';
-const margin = '20px';
+import readable from '../images/readable.png';
+import book from '../images/book.png';
+import expo from '../images/expoo.jpg';
 const data = {
-  skills: ['HTML', 'CSS', 'Javascript', 'React', 'Redux', 'Ruby on rails', 'Angular', 'Ember'],
+  skills: ['HTML', 'CSS', 'ES6', 'React', 'Redux', 'Angular'],
+  education: {
+    school: [
+      {
+        degree: 'Bachelor of Laws',
+        school: 'Can Tho University',
+        year: '2009 - 2013',
+        country: 'Viet Nam'
+      }
+    ],
+    certificate: [
+      {
+        certificate: 'Intro programing nanodegree',
+        school: 'Udacity',
+        year: 2016,
+        country: 'American'
+      },
+      {
+        certificate: 'React Nanodegree programing',
+        school: 'Udacity',
+        year: 2017,
+        country: 'American'
+      },
+      {
+        certificate: 'Frontend developer nanodegree programing',
+        school: 'Udacity',
+        year: 2018,
+        country: 'American'
+      }
+    ]
+  },
   projects: [
     {
       id: 1,
-      name: 'readable',
-      link: 'readable',
-      image: matching,
-      styles: {},
-      skills: ['Redux','Fetch API', 'Babel', 'Webpack', 'NPM', 'Git' ]
+      name: 'neighborhood map',
+      link: 'neighborhoodmap',
+      image: map,
+      skills: ['React', 'FourSquare API', 'NPM', 'Git' ],
     },
     {
       id: 2,
       name: 'memory game',
       link: "memorygame",
       image: matching,
-      styles: {},
       skills: ['HTML', 'CSS', 'Javascript']
     },
     {
       id: 3,
       name: 'book tracking app',
       link: 'booktracking',
-      image: matching,
-      styles: {},
-      skills: ['React', 'HTML', 'CSS', 'ES6 JavaScript', 'Asynchronous JavaScript', 'Fetch API', 'Babel', 'Webpack' ]
+      image: book,
+      skills: ['React', 'ES6', 'Fetch API', 'Babel' ]
     },
     {
       id: 4,
-      name: 'neighborhood map',
-      link: 'neighborhoodmap',
-      image: map,
-      skills: ['React', 'FourSquare API', 'Fetch API', 'Babel', 'NPM', 'Git' ]
+      name: 'mobile flashcard',
+      link: 'mobileflashcard',
+      image: expo,
+      skills: ['React Native', 'Babel', 'Expo', 'Git' ],
+
     },
     {
       id: 5,
-      name: 'mobile flashcard',
-      link: 'mobileflashcard',
-      image: matching,
-      styles: {},
-      skills: ['React Native', 'NPM', 'Babel', 'Expo', 'Git' ]
+      name: 'readable',
+      link: 'readable',
+      image: readable,
+      skills: ['Redux','Fetch API', 'Babel', 'Webpack']
     }
   ]
 }
