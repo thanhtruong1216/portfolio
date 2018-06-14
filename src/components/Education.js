7e95998c5101d534cb42b4286dea34056a2be097
Thanh Thanh <thanhtruong1216@gmail.com>
2018-06-14 21:17:27 +0700

    Feat: Create education component

---
+++ src/components/Education.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import '../styles/Education.css'
+import data from './Data';
+
+class Education extends Component {
+  render () {
+    return (
+      <section className="education-container">
+        <h1>Education</h1>
+        <div>
+          <h2>School</h2>
+          {data.education.school.map(school => {
+            return (
+              <div className="degree">
+                <div className="flex-box">
+                  <div>{school.degree}</div>
+                  <div>{school.year}</div>
+                </div>
+                <div className="flex-box">
+                  <div>{school.school}</div>
+                  <div>{school.country}</div>
+                </div>
+              </div>
+            )
+          })}
+        </div>
+        <div>
+          <h2>Certificate</h2>
+          {data.education.certificate.map(school => {
+            return (
+              <div className="certificate">
+                <div className="flex-box">
+                  <div>{school.certificate}</div>
+                  <div>{school.year}</div>
+                </div>
+                <div className="flex-box">
+                  <div>{school.school}</div>
+                  <div>{school.country}</div>
+                </div>
+              </div>
+            )
+          })}
+        </div>
+      </section>
+    );
+  }
+}
+
+export default Education;
