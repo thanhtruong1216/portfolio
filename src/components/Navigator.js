60aaa4afc374ffb22c2fcef6fba337d1b0447b95
Thanh Thanh <thanhtruong1216@gmail.com>
2019-01-05 21:51:06 +0700

    Style: Links

--- src/components/Navigator.js
+++ src/components/Navigator.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-
+import '../styles/Navigator.sass';
 class Navigator extends Component {
   render() {
     return (
-      <div className="about-me">
+      <div className="navigator">
         <Link to="/home">Home</Link>
         <Link to="/about-me">About me</Link>
         <Link to="/projects">Projects</Link>
