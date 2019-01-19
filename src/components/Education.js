cc246b8810119358ed61b5f1f1d01b5c3a811b95
Thanh Thanh <thanhtruong1216@gmail.com>
2019-01-19 22:06:12 +0700

    Style: Education page

--- src/components/Education.js
+++ src/components/Education.js
@@ -1,22 +1,23 @@
 import React, { Component } from 'react';
-import '../styles/Education.sass'
+import '../styles/Education.sass';
+import '../styles/Experiences.sass'
 import data from './Data';
 
 class Education extends Component {
   render () {
     return (
-      <section className="education-container">
+      <section className="education">
         <h1>Educations & Experiences</h1>
-        <div className="education-content-wrapper">
+        <div className="education__content-wrapper">
           <div>
             <h3>Degree</h3>
             {data.education.school.map((school,key) => (
-              <div className="degree" key={key}>
-                <div className="flex-box">
+              <div className="education__degree" key={key}>
+                <div className="education__flex-box">
                   <p>{school.degree}</p>
                   <p>{school.year}</p>
                 </div>
-                <div className="flex-box">
+                <div className="education__flex-box">
                   <p>{school.school}</p>
                   <div></div>
                 </div>
@@ -26,33 +27,33 @@ class Education extends Component {
           <div>
             <h3>Certificate(Online course)</h3>
             {data.education.certificate.map((school,key) => (
-              <div className="certificate" key={key}>
-                <div className="flex-box">
+              <div className="education__certificates" key={key}>
+                <div className="education__flex-box">
                   <p>{school.certificate}</p>
                   <p>{school.year}</p>
                 </div>
-                <div className="flex-box">
+                <div className="education__flex-box">
                   <p><a href={school.link}>See certificate</a></p>
                   <p>{school.school}</p>
                 </div>
               </div>
             ))}
           </div>
-          <div>
-          <h3>Experiences</h3>
-          {data.experiences.map((experience, key) => (
-            <div className="experience" key={key}>
-              <div className="flex-box">
-                <p>{experience.job}</p>
-                <p>{experience.year}</p>
+          <div className="experiences">
+            <h3>Experiences</h3>
+            {data.experiences.map((experience, key) => (
+              <div className="experiences__content" key={key}>
+                <div className="education__flex-box">
+                  <p>{experience.job}</p>
+                  <p>{experience.year}</p>
+                </div>
+                <div className="education__flex-box">
+                  <div></div>
+                  <p>{experience.company}</p>
+                </div>
               </div>
-              <div className="flex-box">
-                <div></div>
-                <p>{experience.company}</p>
-              </div>
-            </div>
-          ))}
-        </div>
+            ))}
+          </div>
       </div>
     </section>
     );
