ecc5540349afd623c7c607a9bb613f957cdab110
Thanh Thanh <thanhtruong1216@gmail.com>
2019-01-23 12:52:09 +0700

    Chore: Remove chat box

--- src/App.js
+++ src/App.js
@@ -26,21 +26,21 @@ class App extends Component {
   //   })
   // }
 
-  handleNewUserMessage = (content) => {
-    const config = {
-      headers: {'Access-Control-Allow-Origin': '*'}
-    }
-    console.log('sss', content)
-    axios.post('http://localhost:3000/messages/', { message: {content} }, config)
-    .then(response => {
-        console.log(response)
-        const messages = [ ...this.state.messages, response.data ]
-        this.setState({messages})
-    })
-    .catch(error => {
-        console.log(error)
-    })
-  }
+  // handleNewUserMessage = (content) => {
+  //   const config = {
+  //     headers: {'Access-Control-Allow-Origin': '*'}
+  //   }
+  //   console.log('sss', content)
+  //   axios.post('http://localhost:3000/messages/', { message: {content} }, config)
+  //   .then(response => {
+  //       console.log(response)
+  //       const messages = [ ...this.state.messages, response.data ]
+  //       this.setState({messages})
+  //   })
+  //   .catch(error => {
+  //       console.log(error)
+  //   })
+  // }
 
   render() {
     return (
@@ -51,12 +51,12 @@ class App extends Component {
           <NavLink activeClassName="active-nav" to="/projects">Projects</NavLink>
           <NavLink activeClassName="active-nav" to="/thanh-locations">Thanh's locations</NavLink>
         </div>
-        <Widget
+        {/* <Widget
           handleNewUserMessage={this.handleNewUserMessage}
           profileAvatar={avatar}
           title="Welcome to Thanh's house"
           subtitle="Leave with a message and contact info">
-          </Widget>
+          </Widget> */}
         <Heart />
       </section>
     );
